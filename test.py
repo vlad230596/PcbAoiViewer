b83fdf4f78e3aec0f08e92d63d74c0628f5f1cbe
vlad230596 <vlad230596@mail.ru>
2024-05-14 01:03:22 +0300

    Implement stitching line by one pair.

--- test.py
+++ test.py
@@ -23,6 +23,73 @@ from PlaneStitcher import PlaneStitcher
 
 def processLine(path):
     imagesPath = glob.glob(f'{path}/*.png')
+
+    images = [imageUtils.getScaledImage(cv2.imread(path), 1080) for path in sorted(imagesPath)]
+
+    finalResults = None
+    prevX, prevY = 0, 0
+    for i in range(1, len(images)):
+        print(f'\nProcessed: {i - 1} - > {i}')
+        prev = images[i - 1]
+        current = images[i]
+
+        cv2.imshow('prev', prev)
+        cv2.imshow('current', current)
+
+
+        start_time = time.time()
+        result = PlaneStitcher.stitch([prev, current])
+        if result.status:
+            print(f"Elapsed: {time.time() - start_time}")
+            patchedResult = result
+            patchedResult.corners = [((x + prevX), (y + prevY)) for x, y in patchedResult.corners]
+
+            prevX, prevY = patchedResult.corners[-1]
+
+            if finalResults is None:
+                finalResults = patchedResult
+            else:
+                finalResults.masks.append(patchedResult.masks[-1])
+                finalResults.imgs.append(patchedResult.imgs[-1])
+                finalResults.corners.append(patchedResult.corners[-1])
+                finalResults.sizes.append(patchedResult.sizes[-1])
+
+            cv2.imshow('panorama', result.panorama)
+
+            blender = Blender(blender_type='multiband')
+
+            blender.prepare(result.corners, result.sizes)
+            for img, mask, corner in zip(result.imgs, result.masks, result.corners):
+                blender.feed(img, mask.get(), corner)
+            blended = blender.blend()
+            cv2.imshow('blended', blended[0])
+
+            print(f'Sizes: {result.sizes} Corners: {result.corners}')
+            for m in result.masks:
+                mask = m.get()
+                print(f'Mask sizes {mask.shape}')
+        else:
+            cv2.imshow('panorama', imageUtils.getBlankImage(10, 10))
+        cv2.waitKey()
+
+
+    blender = Blender(blender_type='multiband')
+
+    blender.prepare(finalResults.corners, finalResults.sizes)
+    for img, mask, corner in zip(finalResults.imgs, finalResults.masks, finalResults.corners):
+        blender.feed(img, mask.get(), corner)
+    blended = blender.blend()
+    cv2.imshow('lineBlended', imageUtils.getScaledImage(blended[0], 640))
+    cv2.imwrite(f'lineBlended.png', blended[0])
+
+    # multi = PlaneStitcher.stitch(allImages)
+    # cv2.imshow('multi', multi.panorama)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+def refStitch(path):
+    imagesPath = glob.glob(f'{path}/*.png')
     dataset = Dataset()
 
     for path in imagesPath:
@@ -34,94 +101,59 @@ def processLine(path):
     dataset.calculateRanges()
     dataset.loadAllImages()
 
-    # for j in range(0, dataset.columnsCount()):
-    for j in range(0, 1):
-        finalResults = None
-        prevX, prevY = 0, 0
-        for i in range(1, dataset.rowsCount()):
-            print(f'\nProcessed: {i - 1} - > {i}')
-            prev = dataset.at(i - 1, j).getImage()
-            current = dataset.at(i, j).getImage()
+    for j in range(0, dataset.columnsCount()):
+    # for j in range(0, 1):
+        refLeftIndex = 4
+        refRightIndex = refLeftIndex + 1
 
-            cv2.imshow('prev', prev)
-            cv2.imshow('current', current)
+        refLeft = dataset.at(refLeftIndex, j).getImage()
+        refRight = dataset.at(refRightIndex, j).getImage()
 
+        cv2.imshow('refLeft', refLeft)
+        cv2.imshow('refRight', refRight)
+        result = PlaneStitcher.stitch([refLeft, refRight])
+        if not result.status:
+            continue
+        cv2.imshow('panorama', result.panorama)
 
-            start_time = time.time()
-            result = PlaneStitcher.stitch([prev, current])
-            if result.status:
-                print(f"Elapsed: {time.time() - start_time}")
-                patchedResult = result
-                patchedResult.corners = [((x + prevX), (y + prevY)) for x, y in patchedResult.corners]
+        targetWidth = refLeft.shape[1]
+        targetHeight = refLeft.shape[0]
+        targetSize = [targetWidth, targetHeight]
 
-                prevX, prevY = patchedResult.corners[-1]
+        finalSizes = [targetSize for _ in range(dataset.rowsCount())]
+        finalImages = [dataset.at(i, j).getImage() for i in range(dataset.rowsCount())]
 
-                if finalResults is None:
-                    finalResults = patchedResult
-                else:
-                    finalResults.masks.append(patchedResult.masks[-1])
-                    finalResults.imgs.append(patchedResult.imgs[-1])
-                    finalResults.corners.append(patchedResult.corners[-1])
-                    finalResults.sizes.append(patchedResult.sizes[-1])
+        refLeftMask = cv2.resize(result.masks[0].get(), targetSize, interpolation=cv2.INTER_LINEAR_EXACT)
+        refRightMask = cv2.resize(result.masks[1].get(), targetSize, interpolation=cv2.INTER_LINEAR_EXACT)
+        middleMask = cv2.bitwise_and(refLeftMask, refRightMask)
 
-                cv2.imshow('panorama', result.panorama)
+        finalMasks = []
+        finalMasks.append(cv2.UMat(refLeftMask))
+        finalMasks.extend([cv2.UMat(middleMask)] * (dataset.rowsCount() - 2))
+        finalMasks.append(cv2.UMat(refRightMask))
 
-                blender = Blender(blender_type='multiband')
-
-                blender.prepare(result.corners, result.sizes)
-                for img, mask, corner in zip(result.imgs, result.masks, result.corners):
-                    blender.feed(img, mask.get(), corner)
-                blended = blender.blend()
-                cv2.imshow('blended', blended[0])
-
-                print(f'Sizes: {result.sizes} Corners: {result.corners}')
-                for m in result.masks:
-                    mask = m.get()
-                    print(f'Mask sizes {mask.shape}')
-            else:
-
-                cv2.imshow('panorama', imageUtils.getBlankImage(10, 10))
-                # width = 0
-                # height = 0
-                # for corner, size in zip(result.corners, result.sizes):
-                #     xSize = corner[0] + size[0]
-                #     ySize = corner[1] + size[1]
-                #     if xSize > width:
-                #         width = xSize
-                #     if ySize > height:
-                #         height = ySize
-                # print(f'panorama w: {result.panorama.shape[1]} h: {result.panorama.shape[0]}')
-                # print(f'my w: {width} h: {height}')
-                # blank = imageUtils.getBlankImage(width, height)
-                # cv2.imshow('blank', blank)
-                # for image, corner, size, mask in zip(result.imgs, result.corners, result.sizes, result.masks):
-                #     m = mask.get()
-                #     mask3 = cv2.cvtColor(m, cv2.COLOR_GRAY2BGR)
-                #     withMask = cv2.bitwise_and(image, mask3)
-                #
-                #     roiPart = blank[corner[1]:corner[1]+size[1],corner[0]:corner[0] + size[0]]
-                #     roiPartUpdated = cv2.bitwise_or(roiPart, withMask)
-                #
-                #     blank[corner[1]:corner[1]+size[1],corner[0]:corner[0] + size[0]] = roiPartUpdated
-                # cv2.imshow('processed', blank)
-
-
-            cv2.waitKey()
-
+        finalCorners = [None] * dataset.rowsCount()
+        finalCorners[refLeftIndex] = result.corners[0]
+        for i in range(refRightIndex, dataset.rowsCount()):
+            x, y = finalCorners[i - 1]
+            nextCorners = (x + result.corners[1][0], y + result.corners[1][1])
+            finalCorners[i] = nextCorners
+        for i in reversed(range(refLeftIndex)):
+            x, y = finalCorners[i + 1]
+            prevCorners = (x - result.corners[1][0], y - result.corners[1][1])
+            finalCorners[i] = prevCorners
 
         blender = Blender(blender_type='multiband')
 
-        blender.prepare(finalResults.corners, finalResults.sizes)
-        for img, mask, corner in zip(finalResults.imgs, finalResults.masks, finalResults.corners):
+        blender.prepare(finalCorners, finalSizes)
+        for img, mask, corner in zip(finalImages, finalMasks, finalCorners):
             blender.feed(img, mask.get(), corner)
-        blended = blender.blend()
-        cv2.imshow('finalBlended', imageUtils.getScaledImage(blended[0], 640))
-        cv2.imwrite(f'finalBlended{j}.png', blended[0])
+        blended, _ = blender.blend()
+        cv2.imshow('finalBlended', imageUtils.getScaledImage(blended, 1920))
+        cv2.imwrite(f'finalBlended{j}.png', blended)
+        cv2.waitKey()
 
-    # multi = PlaneStitcher.stitch(allImages)
-    # cv2.imshow('multi', multi.panorama)
-
-    cv2.waitKey()
     cv2.destroyAllWindows()
 
-processLine(f'withLight/oneline')
+# refStitch(f'withLight/captured_80.0')
+processLine(f'FinalBlended')
